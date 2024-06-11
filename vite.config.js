d6bb4386c37f402834bc8ecd47d2952f133859b7
JordanManoh <jordan.manoh@gmail.com>
2024-06-11 08:53:19 +0700

    Added assets and html structure

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 export default defineConfig({
-  root: './src',
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: '../build',
-    emptyOutDir: true,
-  },
+    root: './src',
+    server: {
+        port: 3000,
+    },
+    build: {
+        outDir: '../build',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: './src/index.html',
+            },
+        },
+    },
+    plugins: [
+        ViteMinifyPlugin(),
+        injectHTML(),
+        ViteImageOptimizer({
+            png: {
+                quality: 80,
+            },
+            jpeg: {
+                quality: 80,
+            },
+            jpg: {
+                quality: 80,
+            },
+        }),
+    ],
 });
