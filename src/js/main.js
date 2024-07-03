e2d9471c09372c310e794fdbfa33e90ad53a6d13
JordanManoh <jordan.manoh@gmail.com>
2024-07-03 22:12:01 +0300

    Finished hero section

--- src/js/main.js
+++ src/js/main.js
@@ -21,11 +21,15 @@ document.addEventListener('DOMContentLoaded', function () {
             if (currentScroll > lastScrollTop) {
                 // Скролл вниз
                 menu.setAttribute('data-state', 'hidden');
-            } else {
+            } else if (currentScroll < lastScrollTop && currentScroll > 0) {
                 // Скролл вверх
+                menu.setAttribute('data-state', 'visible');
+            } else if (currentScroll === 0) {
+                // В начале страницы
                 menu.setAttribute('data-state', 'default');
             }
 
+            // Обновляем lastScrollTop
             lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Для мобильных или отрицательных значений
         },
         false,
